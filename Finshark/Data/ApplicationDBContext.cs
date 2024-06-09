5db31417434c7f94857db099cd51d8b84b5c3442
Rj <yeeef25@gmail.com>
2024-06-09 10:22:30 -0400

    lfg

---
+++ Finshark/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Finshark.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finshark.Data;
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions dbContextOptions)
+    : base(dbContextOptions)
+    {
+        
+    }
+
+
+    public DbSet<Stock> Stock { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
---
+++ Finshark/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace Finshark.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Subject { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreationTime { get; set; } = DateTime.Now;
+    //links Comment to Stock
+    public int? StockId { get; set; }
+    //Navigation Property
+    public Stock? Stock { get; set; }
+
+}
